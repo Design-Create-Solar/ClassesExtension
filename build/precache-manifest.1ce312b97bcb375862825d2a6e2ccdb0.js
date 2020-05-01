9ead2fff7181d662796d86aed8ce4354c08c4a85
Jessica Ho <jessicaho@Jessicas-MacBook-Pro-3.local>
2020-04-30 19:02:20 -0700

    base extension

---
+++ build/precache-manifest.1ce312b97bcb375862825d2a6e2ccdb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82337b786639d3ed6531e1849754c3ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d5a726324faf612315a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "136fbce5c269cdc3d18b",
+    "url": "/static/js/2.d5699ad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5699ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5a726324faf612315a",
+    "url": "/static/js/main.bc3f232f.chunk.js"
+  },
+  {
+    "revision": "b864580ff6db3b47f20c",
+    "url": "/static/js/runtime-main.5e0e29a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
