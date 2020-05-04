3b65252c0e5880f659bf198e78521bc9a3f48085
Ted <ted@yarmoski.com>
2020-05-04 13:25:34 -0700

    Get all primary info

--- src/classes.js
+++ src/classes.js
@@ -1,5 +1,35 @@
 const puppeteer = require("puppeteer");
 
+//I dont know how to use this inside the evaluate function
+function getPrimaryInfo(obj, element) {
+  obj.title = element.querySelector("h3").textContent;
+  obj.spots = element
+    .querySelector("div.primary-row")
+    .querySelector("div.statusColumn > p")
+    .innerHTML.split("<br>")[1];
+  obj.waitlist = element
+    .querySelector("div.primary-row")
+    .querySelector("div.waitlistColumn > p").textContent;
+  obj.days = element
+    .querySelector("div.primary-row")
+    .querySelector("div.dayColumn a").textContent;
+  obj.time = element
+    .querySelector("div.primary-row")
+    .querySelector("div.timeColumn > p").textContent;
+  obj.location = element
+    .querySelector("div.primary-row")
+    .querySelector("div.locationColumn > p")
+    .textContent.trim()
+    .replace("\n", "");
+  obj.units = element
+    .querySelector("div.primary-row")
+    .querySelector("div.unitsColumn > p").textContent;
+  //classes with multiple instructors will return 2 names without a space in between the names
+  obj.instructor = element
+    .querySelector("div.primary-row")
+    .querySelector("div.instructorColumn > p").textContent;
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/chromium-browser",
@@ -36,35 +66,51 @@ const puppeteer = require("puppeteer");
 
   //create JSONs
   let titles = await page.evaluate(() => {
-    let classes = [];
+    let classes = []; //array of JSONs
     let elements = document.querySelectorAll("div.class-title");
     for (var element of elements) {
-      let obj = {};
       if (element != null) {
-        obj.title = element.querySelector("h3").textContent;
-        obj.time = element
-          .querySelector("div.primary-row")
-          .querySelector("div.timeColumn > p").textContent;
-
-        let secondarySection = element.querySelector("div.secondarySection");
-        if (secondarySection != null) {
-          let secondaryTimeElements = secondarySection.querySelectorAll(
-            "div.timeColumn > p"
-          );
-          let discussions = [];
-          for (var element of secondaryTimeElements) {
-            discussions.push(element.textContent);
+        let lectures = element.querySelectorAll("div.primary-row");
+        for (var lecture of lectures) {
+          let obj = {};
+          obj.title = element.querySelector("h3").textContent;
+          obj.spots = lecture
+            .querySelector("div.statusColumn > p")
+            .innerHTML.split("<br>")[1];
+          obj.waitlist = lecture.querySelector(
+            "div.waitlistColumn > p"
+          ).textContent;
+          obj.days = lecture.querySelector("div.dayColumn a").textContent;
+          obj.time = lecture.querySelector("div.timeColumn > p").textContent;
+          obj.location = lecture
+            .querySelector("div.locationColumn > p")
+            .textContent.trim()
+            .replace("\n", "");
+          obj.units = lecture.querySelector("div.unitsColumn > p").textContent;
+          //classes with multiple instructors will return 2 names without a space in between the names
+          obj.instructor = lecture.querySelector(
+            "div.instructorColumn > p"
+          ).textContent;
+          //discussions
+          let secondarySection = element.querySelector("div.secondarySection");
+          if (secondarySection != null) {
+            let secondaryTimeElements = secondarySection.querySelectorAll(
+              "div.timeColumn > p"
+            );
+            let discussions = [];
+            for (var element of secondaryTimeElements) {
+              discussions.push(element.textContent);
+            }
+            obj.discussions = discussions;
           }
-          obj.discussions = discussions;
+          classes.push(obj);
         }
       }
-      classes.push(obj);
     }
     return classes;
   });
 
   console.log(titles);
-
   console.log("Success!");
   await browser.close();
 })();
