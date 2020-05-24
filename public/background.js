a2cd9a5e54e37640eb41a26651bede09a1a62eee
Jessica Ho <jessicaho@Jessicas-MacBook-Pro-3.local>
2020-05-23 23:59:56 -0700

    popup rerender when page updates

--- public/background.js
+++
@@ -1,7 +0,0 @@
-// chrome.runtime.onMessage.addListener((msg, sender) => {
-//     // validate msg structure
-//     if ((msg.from === "content") && (msg.subject === "showBrowserAction")) {
-//         // enable browser action for requesting tab
-//         chrome.browserAction.show(sender.tab.id)
-//     }
-// })
--- public/content.js
+++ public/content.js
@@ -1,3 +1,7 @@
+// Some credit goes to "Easy Bruinwalk Ratings" Chrome Extension team 
+// @RobertUrsua and @preethamrn on Github
+// for hint on how to get app to rerender once background page updates
+
 function splitEasyTimes(stringBoi) {
     var reg = stringBoi.match(/[a-z]+|[^a-z]+/gi)
     // will split into arr of two elems: number time and am/pm
@@ -16,7 +20,21 @@ function splitEasyTimes(stringBoi) {
     return finalTimeForm
 }
 
-function getClassesFromDOM() {
+function getDays() {
+    let days = jQuery.makeArray($("a.uit-clickover-bottom").filter(function () {
+        return this.innerText.match("M|T|W|R|F")
+    }))
+    return days
+}
+
+function getTimes() {
+    let times = jQuery.makeArray($("td").filter(function () {
+        return this.innerText.match("^[0-9].*m$")
+    }))
+    return times
+}
+
+function getTimesFromDOM() {
     // wanna put jsons formatted as: 
     //  ["M", "W"]
     //  gte: 5
@@ -24,12 +42,8 @@ function getClassesFromDOM() {
     // for each class/dis
     // into one big array
 
-    let days = jQuery.makeArray($("a.uit-clickover-bottom").filter(function () {
-                    return this.innerText.match("M|T|W|R|F")
-                }))
-    let times = jQuery.makeArray($("td").filter(function () {
-                    return this.innerText.match("^[0-9].*m$")
-                }))
+    let days = getDays()
+    let times = getTimes()
     var finalArr = []
     var i, j
     for (i = 0; i < days.length; i++) {
@@ -51,44 +65,77 @@ function getClassesFromDOM() {
         finalArr.push(timeSlot)
     }
     return finalArr
-    {
-        // let classBoxes = document.querySelectorAll("div.planneritembox")
+}
 
-        // var classes = []
-        // for (var box of classBoxes) {
-        //     let content = box.innerText.split("\n")
-        //     let name = content[0]
-        //     let lecOrDis = content[1].split(" ")
-        //     let type
-        //     if (lecOrDis[0]=="Lec")
-        //         type = "lec"
-        //     else 
-        //         type = "dis"
-        //     let lecOrDisNum = lecOrDis[1]
-        //     let obj = { name, type, lecOrDisNum }
-        //     classes.push(obj)
-        // }
-    
-        // return classes
+function getClassesFromDOM() {
+    // want class names, days, and times
+    let classTitles = document.querySelectorAll("td.SubjectAreaName_ClassName > p")
+    // every even index, including 0, will look like "Class #: Subject"
+    // every odd index will look like "180 - Introduction to Algorithms and Complexity"
+
+    // for now want UI table to look like
+    /* Existing Schedule
+        ___________________________________________________________
+       | days | time | type (dis/lec/sem) | class subject and num |
+       | ...                                                      |
+       |__________________________________________________________|
+    */
+    let types = document.querySelectorAll("td.section-header > a")
+    let days = getDays()
+    let times = getTimes()
+    var i, sub, num, subNum, type, j=-1;
+    var res = []; // [ { sub: , classNum: , days, time, type }, { ... }]
+    var subNumArr = []
+
+    for (i = 0; i < classTitles.length; i++) {
+        if (i % 2 == 0)  // even -- sub name
+            sub = classTitles[i].innerText.split(": ")[1]
+        else { // odd -- class num 
+            num = classTitles[i].innerText.split(" - ")[0]
+            subNum = sub + " " + num
+            subNumArr.push(subNum)
+        }
     }
+    for (i = 0; i < days.length; i++) {
+        var slotJson = {}
+        slotJson.days = days[i].innerText
+        slotJson.times = times[i].innerText
+        type = types[i].innerText.split(" ")[0]
+        slotJson.type = type
+        if (type != "Dis")
+            j++
+        slotJson.subNum = subNumArr[j]
+        res.push(slotJson)
+    }
+    return res
 }
 
 // listen for msgs from popup
 chrome.runtime.onMessage.addListener((msg, sender, res) => {
     // validate msg structure
-    if ((msg.from === "popup") && (msg.subject === "DOMInfo")) {
+    if ((msg.from === "popup") && (msg.subject === "processed")) {
+        res(getTimesFromDOM())
+    }
+})
+
+chrome.runtime.onMessage.addListener((msg, sender, res) => {
+    // validate msg structure
+    if ((msg.from === "popup") && (msg.subject === "UI")) {
         res(getClassesFromDOM())
     }
 })
 
-// var insertedNodes = []
-// var observer = new MutationObserver(function(mutations) {
-//     mutations.forEach(function(mutation) {
-//         for (var i = 0; i < mutation.addedNodes.length; i++)
-//             insertedNodes.push(mutation.addedNodes[i])
-//     })
-// })
+var timeout = null
+document.addEventListener("DOMSubtreeModified", 
+    function() {
+        if (timeout) clearTimeout(timeout)
+        timeout = setTimeout(listener, 1000)
+    }, false
+)
 
-// var targetNode = document.querySelector("div.daybox")
-// observer.observe(targetNode, { childList: true})
-// console.log(insertedNodes)
+function listener() {
+    chrome.runtime.sendMessage(
+        {from: "content", subject: "newClasses", stuff: getClassesFromDOM()},
+        res => console.log("msg sent")
+    )
+}
