bd8f5fc749000aab885bd24408dd5ccc1414a7c9
Jessica Ho <jho@Jessicas-MacBook-Pro-2.local>
2020-05-25 04:37:26 -0700

    injection

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# ClassesExtension
+# How to Inject a React app into a Chrome Extension as a content script
 
-For people trying to run: 
-1. Do ```npm run build``` after each edit
-2. Go to chrome://extensions/ in browser
-3. Go into Developer Mode
-4. Click on "Load Unpacked"
-5. Navigate to build folder and click OK on that
+![screenshot](/screenshot.png)
+
+## [Read the tutorial on Medium](https://medium.com/@yosevu/how-to-inject-a-react-app-into-a-chrome-extension-as-a-content-script-3a038f611067)
+
+## Installation
+
+- Fork or clone `https://github.com/bantuist/react-content-script.git`
+- `yarn run install`
+- `yarn run build`
+- Navigate to `chrome://extensions`
+- Check the _Developer mode_ checkbox
+- Click _Load unpacked extension..._
+- Find and select the `build` folder
+- Navigate to `https://www.google.com` 
+
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 /* global chrome */
 import React, { Component } from 'react';
 import './App.css'; 
+import jquery from 'jquery';
+window.$ = window.jQuery=jquery;
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      site: '',
       existingClasses: [], // unprocessed -- for UI
         /*  [{ days: "", times: "", type, subNum: ""}, { ... }] */
       processedExistingClasses: [],
@@ -17,40 +18,121 @@ class App extends Component {
           gte: 5
           lte: 6
         */
-      visible: true
     }
   }
 
   componentDidMount() {
-    chrome.tabs.query({ active: true, currentWindow: true },
-    tabs => {
-      const url = new URL(tabs[0].url)
-      const site = url.href
-      this.setState({
-        site: site
-      })
-      chrome.tabs.sendMessage( 
-        tabs[0].id, 
-        {from: "popup", subject: "processed"}, 
-        res => this.setState({processedExistingClasses: res })
-      )
-      chrome.tabs.sendMessage( 
-        tabs[0].id, 
-        {from: "popup", subject: "UI"}, 
-        res => this.setState({existingClasses: res })
-      )
-      // chrome.runtime.onMessage.addListener((msg, sender, res) => {
-      //   if ((msg.from === "content") && (msg.subject === "newProcessedClasses")) {
-      //     // TODO
-      //   }
-      // })
-      chrome.runtime.onMessage.addListener((msg, sender, res) => {
-        if ((msg.from === "content") && (msg.subject === "newClasses")) {
-          this.setState({existingClasses: msg.stuff})
-          res("message received")
+    this.setState({
+      existingClasses: this.getClassesFromDOM(),
+      processedExistingClasses: this.getTimesFromDOM()
+    })
+  }
+  splitEasyTimes(stringBoi) {
+    var reg = stringBoi.match(/[a-z]+|[^a-z]+/gi)
+    // will split into arr of two elems: number time and am/pm
+    // goal: get rid of the : in the 1st elem 
+    // in addition to taking care of 24 hr conversion
+    
+    let splitColonTime = reg[0].split(":")
+    if (reg[1] == "pm")
+       splitColonTime[0] = (Number(splitColonTime[0])+12).toString()
+
+    // splitColonTime is array of either one or two elems
+    var finalTimeForm = splitColonTime[0]
+    if (splitColonTime.length == 2) 
+        finalTimeForm = splitColonTime[0] + splitColonTime[1]
+    
+    return finalTimeForm
+  }
+
+  getDays() {
+    let days = window.jQuery.makeArray(window.$("a.uit-clickover-bottom").filter(function () {
+        return this.innerText.match("M|T|W|R|F")
+    }))
+    return days
+  }
+
+  getTimes() {
+    let times = window.jQuery.makeArray(window.$("td").filter(function () {
+        return this.innerText.match("^[0-9].*m$")
+    }))
+    return times
+  }
+
+  getTimesFromDOM() {
+    // wanna put jsons formatted as: 
+    //  ["M", "W"]
+    //  gte: 5
+    //  lte: 6
+    // for each class/dis
+    // into one big array
+
+    let days = this.getDays()
+    let times = this.getTimes()
+    var finalArr = []
+    var i, j
+    for (i = 0; i < days.length; i++) {
+        let thingy = times[i].innerText.split("-")
+        let start = thingy[0]
+        let end = thingy[1]
+        let processedStart = this.splitEasyTimes(start)
+        let processedEnd = this.splitEasyTimes(end)
+
+        var daysArr = []
+        var daysStr = days[i].innerText
+        for (j=0; j<daysStr.length; j++)
+            daysArr.push(daysStr[j])
+        
+        var timeSlot = {}
+        timeSlot.days = daysArr
+        timeSlot.gte = processedStart
+        timeSlot.lte = processedEnd
+        finalArr.push(timeSlot)
+    }
+    return finalArr
+  }
+
+  getClassesFromDOM() {
+    // want class names, days, and times
+    let classTitles = document.querySelectorAll("td.SubjectAreaName_ClassName > p")
+    // every even index, including 0, will look like "Class #: Subject"
+    // every odd index will look like "180 - Introduction to Algorithms and Complexity"
+
+    // for now want UI table to look like
+    /* Existing Schedule
+        ___________________________________________________________
+       | days | time | type (dis/lec/sem) | class subject and num |
+       | ...                                                      |
+       |__________________________________________________________|
+    */
+    let types = document.querySelectorAll("td.section-header > a")
+    let days = this.getDays()
+    let times = this.getTimes()
+    var i, sub, num, subNum, type, j=-1;
+    var res = []; // [ { sub: , classNum: , days, time, type }, { ... }]
+    var subNumArr = []
+
+    for (i = 0; i < classTitles.length; i++) {
+        if (i % 2 == 0)  // even -- sub name
+            sub = classTitles[i].innerText.split(": ")[1]
+        else { // odd -- class num 
+            num = classTitles[i].innerText.split(" - ")[0]
+            subNum = sub + " " + num
+            subNumArr.push(subNum)
         }
-      })
-    })  
+    }
+    for (i = 0; i < days.length; i++) {
+        var slotJson = {}
+        slotJson.days = days[i].innerText
+        slotJson.times = times[i].innerText
+        type = types[i].innerText.split(" ")[0]
+        slotJson.type = type
+        if (type != "Dis")
+            j++
+        slotJson.subNum = subNumArr[j]
+        res.push(slotJson)
+    }
+    return res
   }
 
   renderExistingClassesData() {
@@ -68,33 +150,18 @@ class App extends Component {
   }
 
   render() {
-    let correctSite;
-
-    if (this.state.site=="https://be.my.ucla.edu/ClassPlanner/ClassPlan.aspx") 
-      correctSite = true
-    else 
-      correctSite = false
-
     console.log(this.state)
     return (
       <div className="App">
-        {
-          correctSite
-          ? <div>
-              <button type="button">X</button>
-              <form >
-                <input type="text" placeholder="Enter a Subject (ex: COM SCI)" required />
-                <button type="submit">Go!</button>
-              </form>
-              <table id="existing-classes">
-                <tbody>
-                  {this.renderExistingClassesData()}
-                </tbody>
-              </table>
-            </div>
-          : <h1>Go to your UCLA class planner.</h1>
-        }
-
+        <form >
+          <input type="text" placeholder="Enter a Subject (ex: COM SCI)" required />
+          <button type="submit">Go!</button>
+        </form>
+        <table id="existing-classes">
+          <tbody>
+            {this.renderExistingClassesData()}
+          </tbody>
+        </table>
       </div>
     )
   }
